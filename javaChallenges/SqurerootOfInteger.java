1c7e5e8ad8ec13c3e05df4359b460f4a386e9186
RamyaaSridhar <119778220+RamyaaSridhar@users.noreply.github.com>
2022-12-07 12:32:19 +0530

    Add files via upload

---
+++ javaChallenges/SqurerootOfInteger.java
@@ -0,0 +1,15 @@
+package javaChallenges;
+import java.util.Scanner;
+import static java.lang.Math.*;
+public class SqurerootOfInteger {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int res = (int) Math.sqrt(num);
+		System.out.println(res);
+		
+
+	}
+
+}
