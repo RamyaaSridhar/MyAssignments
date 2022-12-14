483a54a226637cac17fefbdf0fe76189e5e1a59f
RamyaaSridhar <119778220+RamyaaSridhar@users.noreply.github.com>
2022-12-14 20:27:50 +0530

    Add files via upload

---
+++ javaChallenges/RepeatedElement.java
@@ -0,0 +1,32 @@
+package javaChallenges;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class RepeatedElement {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String values = sc.nextLine();
+		String [] input = values.split(" ");
+		int count =0;
+		for(int i=0;i<input.length;i++) {
+			for(int j=i+1;j<input.length;j++) {
+				if(input[i].equals(input[j])) {
+					count+=1;
+				}
+			}
+			if(count>1) {
+				System.out.println("true");
+				break;
+				
+			}
+		}
+		if (count<1) {
+			System.out.println("false");
+		}
+
+	}
+
+}
