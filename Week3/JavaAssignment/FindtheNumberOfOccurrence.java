b8bf1c9ad654959ed0247edb3d64bf6ae65e4b1d
RamyaaSridhar <119778220+RamyaaSridhar@users.noreply.github.com>
2022-12-23 10:03:45 +0530

    Add files via upload

--- Week3/JavaAssignment/FindtheNumberOfOccurrence.java
+++ Week3/JavaAssignment/FindtheNumberOfOccurrence.java
@@ -1,6 +1,7 @@
 package Week3.JavaAssignment;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -8,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.TreeMap;
 
 public class FindtheNumberOfOccurrence {
 
@@ -15,7 +17,8 @@ public class FindtheNumberOfOccurrence {
 		
 		int [] array = {2,3,5,6,3,2,1,4,2,1,6,-1};
 		
-		Map<Integer,Integer> hashMap = new HashMap<Integer,Integer>();
+		Map<Integer,Integer> hashMap = new TreeMap<Integer,Integer>(); //{}
+	
 		for(int i=0;i<array.length;i++) {
 			if(!hashMap.containsKey(array[i])) {
 				hashMap.put(array[i],1);
@@ -25,22 +28,34 @@ public class FindtheNumberOfOccurrence {
 				hashMap.put(array[i], integer+1);
 			}
 		}
+		System.out.println(hashMap);
 		
+		//----------------------------Ascending order-------------------------------------------------------------
 		
-		//sorting the map
-				Set<Entry<Integer, Integer>> entrySet = hashMap.entrySet();
-				List<Entry<Integer, Integer>>list = new ArrayList<>(entrySet);    //Convert Map into List to perform Sorting
-				Collections.sort(list, new Comparator <Entry<Integer, Integer>> () {
-
-					@Override
-					public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
-						
-						return o1.getKey().compareTo(o2.getKey());
-					}
-	});
-				for(int i=0;i<list.size();i++) {
-					System.out.print(list.get(i).getKey()+"->"+list.get(i).getValue()+","+" ");
-				}
-				
+		Set<Entry<Integer, Integer>> entrySet = hashMap.entrySet();
+		
+		for(Entry<Integer,Integer> val : entrySet)
+        {
+            System.out.print(val.getKey() + "-->" + val.getValue()+",");
+        }
+		System.out.println();
+		
+		//------------------------------Descending Order------------------------------------------------------
+		Set<Integer> keySet = hashMap.keySet();
+		Object[] keys = keySet.toArray();
+		Collection<Integer> value = hashMap.values();
+		Object[] values = value.toArray();
+		
+		for(int i=(keys.length)-1;i>=0;i--) {
+			if(i!=0) {
+				System.out.print(keys[i]+"-->"+values[i]+",");
+			}
+			else {
+				System.out.print(keys[i]+"-->"+values[i]);
+			}
+		}
+		
 	}
+
+	
 }
