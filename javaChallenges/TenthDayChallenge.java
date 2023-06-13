413b1084b346946b5e312f36a7d5aecac698def6
RamyaaSridhar <119778220+RamyaaSridhar@users.noreply.github.com>
2023-06-13 18:15:57 +0530

    Update TenthDayChallenge.java

--- javaChallenges/TenthDayChallenge.java
+++ javaChallenges/TenthDayChallenge.java
@@ -6,6 +6,7 @@ public class TenthDayChallenge {
 
 	public static void main(String[] args) {
 		
+		
 		Scanner sc = new Scanner(System.in);
 		String a = sc.nextLine();
 		String b = sc.nextLine();
