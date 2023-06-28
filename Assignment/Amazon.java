30c2ad01bf502668cc0a62fc3c385c2dc979e41c
RamyaaSridhar <119778220+RamyaaSridhar@users.noreply.github.com>
2023-06-28 13:59:55 +0530

    Update Amazon.java

--- Assignment/Amazon.java
+++ Assignment/Amazon.java
@@ -2,7 +2,6 @@
  * 1.Load the URL https://www.amazon.in/
  * 2.search as oneplus 9 pro 
  * 3.Get the price of the first product
- * 4. Print the number of customer ratings for the first displayed product
  * 5. Click the first text link of the first image
  *hello
 */
